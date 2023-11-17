500a795435898d216a2b0cda2c68617d8a82d6c1
Amene Terhemen <ameneterh@gmail.com>
2023-11-17 23:04:11 +0100

    connect to database

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -38,43 +38,57 @@ function Header() {
     },
   ];
   return (
-    <div className="box-border flex items-center max-w-screen justify-between border-b-[1px] mx-auto px-[20px] sticky top-0 text-black bg-white">
-      <div className="w-[120px] h-[80px] text-white font-extrabold text-[60px] flex justify-center items-center">
-        <img
-          src="/ewatomi_logo3.png"
-          className="w-[120px] h-[60px] object-cover"
-        />
-      </div>
-      <div className="box-border hidden md:flex gap-12 mr-8">
-        {menu.map((item) => (
-          <div
-            key={item.id}
-            className="cursor-pointer hover:underline decoration-[2px] hover:underline-offset-[16px] font-medium"
-          >
-            <Link to={item.link}>
-              <h2>{item.name}</h2>
-            </Link>
-          </div>
-        ))}
-      </div>
-      <div
-        onClick={() => setNav(!nav)}
-        className="w-[90px] h-[90px] z-10 flex justify-center items-center cursor-pointer md:hidden"
-      >
-        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-      </div>
-
-      {nav && (
-        <div className="flex flex-col justify-center items-center w-full absolute top-0 left-0 h-screen bg-gradient-to-b from-red-700 to-red-200 opacity-90 gap-12 text-white font-extrabold">
+    <div className="max-w-screen flex flex-col">
+      <div className="box-border flex items-center w-full justify-between border-b-[1px] mx-auto px-[20px] sticky top-0 text-black bg-white">
+        <div className="w-[120px] h-[80px] text-white font-extrabold text-[60px] flex justify-center items-center">
+          <img
+            src="/ewatomi_logo3.png"
+            className="w-[120px] h-[60px] object-cover"
+          />
+        </div>
+        <div className="box-border hidden md:flex gap-12 mr-8">
           {menu.map((item) => (
-            <div key={item.id} className="cursor-pointer text-3xl">
+            <div
+              key={item.id}
+              className="cursor-pointer hover:underline decoration-[2px] hover:underline-offset-[16px] font-medium"
+            >
               <Link to={item.link}>
                 <h2>{item.name}</h2>
               </Link>
             </div>
           ))}
         </div>
-      )}
+        <div
+          onClick={() => setNav(!nav)}
+          className="w-[90px] h-[90px] z-10 flex justify-center items-center cursor-pointer md:hidden"
+        >
+          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </div>
+
+        {nav && (
+          <div className="flex flex-col justify-center items-center w-full absolute top-0 left-0 h-screen bg-gradient-to-b from-red-700 to-red-200 opacity-90 gap-12 text-white font-extrabold">
+            {menu.map((item) => (
+              <div key={item.id} className="cursor-pointer text-3xl">
+                <Link to={item.link}>
+                  <h2>{item.name}</h2>
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="w-full h-[60px] bg-slate-50 py-2">
+        <form className="w-full md:w-[50%] h-full p-3 mx-auto flex items-center justify-center bg-white rounded-full">
+          <input
+            type="text"
+            placeholder="enter a search term ..."
+            name="search"
+            id="search"
+            className="w-full h-full focus:outline-none"
+          />
+          <FaSearch size={20} className="text-slate-300" />
+        </form>
+      </div>
     </div>
   );
 }
