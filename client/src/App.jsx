b39a7727e72d7d07bc1c7ca046e8c1b204a7fcef
Amene Terhemen <ameneterh@gmail.com>
2023-11-18 08:52:29 +0100

    create test api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import ContactUs from "./Pages/ContactUs";
 import SideBar from "./Components/SideBar";
+import SignUp from "./Pages/Signup";
+import SignIn from "./Pages/Signin";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Header />
       <SideBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact-us" element={<ContactUs />} />
-        {/* <Route path="/sign-up" element={<SignUp />} /> */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
         {/* <Route path="/about" element={<About />} /> */}
         {/* <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
         </Route> */}
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes, FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
   const [nav, setNav] = useState(false);
+  // const { currentUser } = useSelector((state) => state.user);
+  const [currentUser, setCurrentUser] = useState(false);
 
   const menu = [
     {
@@ -31,22 +34,19 @@ function Header() {
       name: "NEW ARRIVALS",
       link: "/new-arrivals",
     },
-    {
-      id: 6,
-      name: "CONTACT US",
-      link: "/contact-us",
-    },
   ];
   return (
     <div className="max-w-screen flex flex-col">
-      <div className="box-border flex items-center w-full justify-between border-b-[1px] mx-auto px-[20px] sticky top-0 text-black bg-white">
+      <div className="flex items-center w-full justify-between border-b-[1px] mx-auto px-[5px] md:px-[40px] sticky top-0 text-black bg-white">
         <div className="w-[120px] h-[80px] text-white font-extrabold text-[60px] flex justify-center items-center">
-          <img
-            src="/ewatomi_logo3.png"
-            className="w-[120px] h-[60px] object-cover"
-          />
+          <Link to="/">
+            <img
+              src="/ewatomi_logo3.png"
+              className="w-[120px] h-[60px] object-cover"
+            />
+          </Link>
         </div>
-        <div className="box-border hidden md:flex gap-12 mr-8">
+        <div className="hidden md:flex gap-12">
           {menu.map((item) => (
             <div
               key={item.id}
@@ -58,18 +58,44 @@ function Header() {
             </div>
           ))}
         </div>
+
         <div
           onClick={() => setNav(!nav)}
           className="w-[90px] h-[90px] z-10 flex justify-center items-center cursor-pointer md:hidden"
         >
-          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+          {nav ? (
+            <FaTimes size={30} className="text-white" />
+          ) : (
+            <FaBars size={30} />
+          )}
+        </div>
+
+        {/* to show picture on login or just login if not logged in */}
+        <div
+          onClick={() => setCurrentUser(!currentUser)}
+          className="cursor-pointer"
+        >
+          {currentUser ? (
+            <img
+              className="rounded-full h-12 w-12 object-cover"
+              src="../new-arrival-1.jpg"
+              alt="profile"
+            />
+          ) : (
+            <Link
+              to="/signin"
+              className="text-slate-800 bg-slate-100 hover:opacity-80 rounded-lg px-4 py-2"
+            >
+              Sign In
+            </Link>
+          )}
         </div>
 
         {nav && (
-          <div className="flex flex-col justify-center items-center w-full absolute top-0 left-0 h-screen bg-gradient-to-b from-red-700 to-red-200 opacity-90 gap-12 text-white font-extrabold">
+          <div className="flex flex-col justify-center items-center w-full absolute top-0 left-0 h-screen bg-gradient-to-b from-black via-black to-gray-200 opacity-90 gap-12 text-white font-bold">
             {menu.map((item) => (
               <div key={item.id} className="cursor-pointer text-3xl">
-                <Link to={item.link}>
+                <Link to={item.link} onClick={() => setNav(!nav)}>
                   <h2>{item.name}</h2>
                 </Link>
               </div>
@@ -78,7 +104,7 @@ function Header() {
         )}
       </div>
       <div className="w-full h-[60px] bg-slate-50 py-2">
-        <form className="w-full md:w-[50%] h-full p-3 mx-auto flex items-center justify-center bg-white rounded-full">
+        <form className="w-full md:w-[50%] h-full p-3 mx-3 md:mx-auto flex items-center justify-center bg-white rounded-full">
           <input
             type="text"
             placeholder="enter a search term ..."
