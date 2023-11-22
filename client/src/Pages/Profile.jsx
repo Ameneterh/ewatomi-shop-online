4db775b63ecf537ac44a61ee835e134284bf8f24
Amene Terhemen <ameneterh@gmail.com>
2023-11-22 02:19:07 +0100

    complete delete user listing functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -139,6 +139,24 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      next(error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -250,7 +268,10 @@ export default function Profile() {
               >
                 <p>{listing.name}</p>
               </Link>
-              <div className="flex flex-col items-center">
+              <div
+                onClick={() => handleListingDelete(listing._id)}
+                className="flex flex-col items-center"
+              >
                 <button className="text-red-700 uppercase">Delete</button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
