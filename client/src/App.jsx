ede4e720fe96cd3efbde1acfd4cf2df56f63a697
Amene Terhemen <ameneterh@gmail.com>
2023-11-25 02:44:49 +0100

    complete about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import CreateListing from "./Pages/CreateListing";
 import UpdateListing from "./Pages/UpdateListing";
 import Listing from "./Pages/Listing";
 import Search from "./Pages/Search";
+import TheBrand from "./Pages/TheBrand";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/the-brand" element={<TheBrand />} />
         {/* <Route path="/contact-us" element={<ContactUs />} /> */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
---
+++ client/src/Pages/TheBrand.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default function TheBrand() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 text-center">
+        About Ewatomi Unique Beauty Place
+      </h1>
+
+      {/* about */}
+      <div>
+        <h2 className="text-2xl font-semibold mb-3 text-slate-800"></h2>
+        <p className="mb-4 text-slate-700">
+          Ewatomi Unique Beauty Place has been involved with the fashion
+          industry for several years. She has been actively marketing fashion
+          products online since 2018 in Ado Ekiti, Ekiti State, Nigeria.
+          Although praised for the quality of many of beauty products she sales,
+          she has attained a special notoriety for her hair and hair care
+          products.
+        </p>
+        <p className="mb-4 text-slate-700">
+          Ewatomi Unique Beauty Place is registered with the Corporate Affairs
+          Commission in August 2020 with registration number BN3171142 and
+          business address as ABUAD Multisystem Hospital, Ado Ekiti, Ekiti
+          State, Nigeria. The company was started to make available quality and
+          affordable hair and beauty products.
+        </p>
+        <p className="mb-4 text-slate-700">
+          The company Chief Executive Officer (CEO), Idowu Oluboba Ewatomi, is a
+          fashionpreneur (fashion entrepreneur)
+        </p>
+      </div>
+
+      {/* mission */}
+      <div>
+        <h2 className="text-2xl font-semibold mb-3 text-slate-800">
+          Mission Statement
+        </h2>
+        <p className="mb-4 text-slate-700">
+          To change the way fashion is thought of by delivering premium designs
+          at radically fair prices; it is our belief that no one needs break the
+          bank to step out in style.
+        </p>
+      </div>
+
+      {/* vision */}
+      <div>
+        <h2 className="text-2xl font-semibold mb-3 text-slate-800">
+          Vision Statement
+        </h2>
+        <p className="mb-4 text-slate-700">
+          To bring fashion to our esteemed customers in an affordable way.
+        </p>
+      </div>
+    </div>
+  );
+}
